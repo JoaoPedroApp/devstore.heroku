1597d6a20c23553f3620e0d3b16ada40237542c9
João <joaopedroapparecido@gmail.com>
2021-09-17 19:03:49 -0300

    Acabado

--- src/index.js
+++ src/index.js
@@ -15,90 +15,62 @@ app.get('/produto', async (req, resp) => {
     }
 })
 
-app.post('/produto', async (req, resp) => {
-    try {
-        let {  nome, categoria, precod, precopor, avaliacao,  estoque, imagem, ativo, dt_inclusao } = req.body;
-
-        if (nome == '' || categoria == '' || precod == '' || precopor == '' || avaliacao == '' ||  estoque == '' || imagem == '' ||  ativo == '' || dt_inclusao == '')
-            return resp.send({ erro: ' Preencha todos os campos!' })
-
-        if (nome.length < 4 )
-            return resp.send({ erro: ' Insira mais que 4 caracteres!' });
-        
-        let ProdutoRepetido = await db.tb_produto.findOne({ where: { nm_produto: nome } })
-        if (ProdutoRepetido != null)
-        return resp.send({ erro: 'O produto já existe nesta turma!' });
-
-        if (precod <= 0 || precopor <= 0 || avaliacao <= 0 || estoque <= 0  )
-            return resp.send({ erro: ' Números negativos não são permitidos' });
-        
-
-        let r = await db.tb_produto.create(
-            {
-            nm_produto: nome,
-            ds_categoria: categoria,
-            vl_preco_de: precod,
-            vl_preco_por: precopor,
-            vl_avaliacao: avaliacao,
-            ds_produto: produto,
-            qtd_estoque: estoque,
-            img_produto: imagem,
-            bt_ativo : ativo,
-            dt_inclusao: new Date()
-
-            }, 
-            {
-                where: { id_produto: id }
-            }
-        )
-
-        resp.send(r);
-    } catch (e) {
-        resp.send({ erro: 'Deu erro' });
-        console.log(e.toString());
-    }
-})
-
 app.put('/produto/:id', async (req, resp) => {
-    try {
-        let { nome, categoria, precod, precopor, avaliacao,  estoque, imagem , ativo } = req.body;
+    try{
+        let { nome, categoria, preco_de, preco_por, avaliacao, estoque, imagem, descricao } = req.body;
         let { id } = req.params;
 
-        if (nome == '' || categoria == '' || precod == '' || precopor == '' || avaliacao == '' ||  estoque == '' || imagem == ''||  ativo == '' || dt_inclusao == '')
-            return resp.send({ erro: ' Preencha todos os campos!' })
-
-        if (nome.length < 5 )
-            return resp.send({ erro: ' Insira mais que 4 caracteres!' });
-
-        let ProdutoRepetido = await db.tb_produto.findOne({ where: { nm_produto: nome} })
-        if ( ProdutoRepetido != null)
-            return resp.send({ erro: 'O número do Produto já existe nesta turma!' });
-  
-        if (precod <= 0 || precopor <= 0 || avaliacao <= 0 || estoque <= 0 )
-            return resp.send({ erro: ' Números negativos não são permitidos' });
-
-
-        let r = await db.tb_produto.update(
+        let b = await db.tb_produto.update(
             {
-            nm_produto: nome,
-            ds_categoria: categoria,
-            vl_preco_de: precod,
-            vl_preco_por: precopor,
-            vl_avaliacao: avaliacao,
-            ds_produto: produto,
-            qtd_estoque: estoque,
-            img_produto: imagem,
-            bt_ativo: ativo,
+                nm_produto: nome,
+                ds_categoria: categoria,
+                vl_preco_de: preco_de,
+                vl_preco_por: preco_por,
+                vl_avaliacao: avaliacao,
+                qtd_estoque: estoque,
+                img_produto: imagem,
+                ds_produto: descricao,
+                bt_ativo: true,
+                dt_inclusao: new Date()
             },
             {
                 where: { id_produto: id }
             }
         )
         resp.sendStatus(200);
-    } catch (e) {
-        resp.send({ erro: e.toString() })
+    } catch(b) {
+        resp.send({ erro: b.toString() })
     }
 })
+
+
+app.post('/produto', async (req, resp) => {
+    try{
+        let produtoexist= await db.tb_produto.findOne({where: {nm_produto: req.body.nome} });
+        if(produtoexist != null) {
+            return resp.send({ erro: 'Produto ja existe!'})
+        } 
+            let { nome, categoria, preco_de, preco_por, avaliacao, estoque, imagem, descricao } = req.body;
+            
+            let b = await db.tb_produto.create({
+                nm_produto: nome,
+                ds_categoria: categoria,
+                vl_preco_de: preco_de,
+                vl_preco_por: preco_por,
+                vl_avaliacao: avaliacao,
+                qtd_estoque: estoque,
+                img_produto: imagem,
+                ds_produto: descricao,
+                bt_ativo: true,
+                dt_inclusao: new Date()
+            })
+            resp.send(b);
+        
+    } catch(b) {
+        resp.send({ erro: b.toString() })
+    }
+})
+
 
 app.delete('/produto/:id', async (req, resp) => {
     try {
@@ -112,4 +84,4 @@ app.delete('/produto/:id', async (req, resp) => {
 
 
 app.listen(process.env.PORT,
-            x => console.log(`Subiui parceiro na porta ${process.env.PORT}`))
+            x => console.log(`Subiu parceiro na porta ${process.env.PORT}`))
